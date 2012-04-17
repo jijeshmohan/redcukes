52147cdc8a4952fa31043ff0ce4fefb03cd2a128
Jijesh Mohan <jijeshmohan@gmail.com>
2012-04-17 07:10:54 +0000

    few refactoring and code documentation

--- spec/lib/redmine_spec.rb
+++ spec/lib/redmine_spec.rb
@@ -2,15 +2,23 @@ require (File.expand_path('./../../spec_helper', __FILE__))
 
 describe Redcukes::Redmine do
 
-  it "should have default attributes" do
+ it "should have default attributes" do
     Redcukes::Redmine.format.should == ActiveResource::Formats::XmlFormat
-  end
+ end
 
-  it "should respond to search filter attribute" do
+ it "should respond to search filter attribute" do
     Redcukes::Redmine.should be_respond_to :search_filter
-  end
+ end
+ 
+ it "should have empty search filter by default" do
+    Redcukes::Redmine.search_filter.should == {}	
+ end
 
-  context "redmine configuration" do
+ it "should have default result status" do
+    Redcukes::Redmine.result_status.should == {:passed => 3,:failed => 6}
+ end
+ 
+ context "redmine configuration" do
 
     it "should respond to configure" do
       Redcukes::Redmine.should be_respond_to :configure
@@ -19,10 +27,13 @@ describe Redcukes::Redmine do
     it "should be able to configure attributes" do
       Redcukes::Redmine.configure do |c|
         c.user= "user"
+		c.result_status = {:passed => 7,:failed => 9}
       end
       Redcukes::Redmine.user.should == "user"
+	  Redcukes::Redmine.result_status.should == {:passed => 7,:failed => 9}
       Redcukes::Redmine.password.should be_nil
     end
-
-  end
+    
+ end
+ 
 end
