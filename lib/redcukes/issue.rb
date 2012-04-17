4ed323bf9456e158a802f42f555323e909784d54
Jijesh Mohan <jijeshmohan@gmail.com>
2012-04-17 22:01:33 +0530

    code refactor

--- lib/redcukes/issue.rb
+++ lib/redcukes/issue.rb
@@ -3,20 +3,24 @@ module Redcukes
 
     @@default_filter = {:status_id => '*'}
 
-    def self.cucumber_features
-      filter = @@default_filter.merge(self.search_filter)
-      find(:all, :params => filter)
-    end
-
-    def self.update_status(id, status={})
-      s = Issue.find(id)
-      return if s.nil?
-      if (status[:failed] != 0 || status[:errors] != 0)
-        s.status_id = self.result_status[:failed]
-      else
-        s.status_id = self.result_status[:passed] if status[:passed] > 0
+    class << self
+      # Get all redmine issues with a specified search criteria
+      def cucumber_features
+        filter = @default_filter.merge(self.search_filter)
+        find(:all, :params => filter)
       end
-      s.save
+
+      # Update redmine issue status.
+      def update_status(id, status={})
+        s = Issue.find(id)
+        return if s.nil?
+        if (status[:failed] != 0 || status[:errors] != 0)
+          s.status_id = self.result_status[:failed]
+        else
+          s.status_id = self.result_status[:passed] if status[:passed] > 0
+        end
+        s.save
+      end
     end
   end
 end
--- lib/redcukes/redmine.rb
+++ lib/redcukes/redmine.rb
@@ -1,44 +1,44 @@
 require 'active_resource'
 
 module Redcukes
-    
+
   # Redmine is the main configuration base class to access redmine application 
   class Redmine < ActiveResource::Base
-  
+
     # active resource format set to xml for calling redmine REST service
     self.format = :xml
-    
+
     class << self
-        # To set result status update back to redmine.
-        # The value should be a has contains :passed and :failed status ids in the redmine
-        def result_status=(value)
-            @result = value
-        end
+      # To set result status update back to redmine.
+      # The value should be a has contains :passed and :failed status ids in the redmine
+      def result_status=(value)
+        @result = value
+      end
 
-        # To get result status update back to redmine
-        # if there is no result status specified, this will be defaulted to  {:passed => 3,:failed => 6}
-        # this means in redmine the issue status will be set to “Resolved” if passed and set to “Rejected” if failed.
-        def result_status
-            @result || {:passed => 3,:failed => 6}
-        end
-        
-        # To set issue search filter in redmine, possible filter are 
-        # 
-        # :project_id, :tracker_id, :status_id
-        def search_filter=(value)
-            @search_content = value
-        end
-        
-        # To get the issue search filter for fetcting issues from redmine.
-        def search_filter
-            @search_content || {}
-        end
+      # To get result status update back to redmine
+      # if there is no result status specified, this will be defaulted to  {:passed => 3,:failed => 6}
+      # this means in redmine the issue status will be set to “Resolved” if passed and set to “Rejected” if failed.
+      def result_status
+        @result || {:passed => 3, :failed => 6}
+      end
 
-        # To configure redmine settings via cucumber env file
-        def configure &block
-            block.call(self)
-        end
+      # To set issue search filter in redmine, possible filter are
+      #
+      # :project_id, :tracker_id, :status_id
+      def search_filter=(value)
+        @search_content = value
+      end
+
+      # To get the issue search filter for fetcting issues from redmine.
+      def search_filter
+        @search_content || {}
+      end
+
+      # To configure redmine settings via cucumber env file
+      def configure &block
+        block.call(self)
+      end
     end
-    
+
   end
 end
